494222018e0c20f0e279afe28c46b96719cc0edc
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-10-11 10:45:02 -0400

    mean, stdv, summarize_dataset methods are created
    Here methods are created for calculating mean, standard deviation and and method to show summary after calculating mean and standard deviation.

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -13,23 +13,27 @@ def sperate_by_class(dataset):
         seperated[class_value].append(datapoint)
     return seperated
 
-# Test separating data by class
-dataset = [[3.393533211,2.331273381,0],
-[3.110073483,1.781539638,0],
-[1.343808831,3.368360954,0],
-[3.582294042,4.67917911,0],
-[2.280362439,2.866990263,0],
-[7.423436942,4.696522875,1],
-[5.745051997,3.533989803,1],
-[9.172168622,2.511101045,1],
-[7.792783481,3.424088941,1],
-[7.939820817,0.791637231,1]]
+##Summarize the data set
+## Finding the mean and standard deviation
+
+#Calculate the mean of a list of numbers
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
 
 
-##Testing the sperate by class method with dataset.
-classes = sperate_by_class(dataset)
-for class_labels in classes:
-    print(class_labels)
-    for obj in classes[class_labels]:
-        print(obj)
-    
+#Calculate the standard deviation of a list of numbers
+def stdev(numbers):
+    average = mean(numbers)
+    variance  = sum((x-average)**2 for x in numbers)/float(len(numbers)-1)
+    return variance**0.5
+
+
+# Summarize a dataset
+def summarize_dataset(dataset):
+    summaries = [(mean(feature), stdev(feature), len(feature)) for feature in zip(*dataset)]
+    del summaries[-1]  # remove the target column
+    return summaries
+
+dataset = [[3.393533211,2.331273381,0], [3.110073483,1.781539638,0], [1.343808831,3.368360954,0], [3.582294042,4.67917911,0], [2.280362439,2.866990263,0], [7.423436942,4.696522875,1], [5.745051997,3.533989803,1], [9.172168622,2.511101045,1], [7.792783481,3.424088941,1], [7.939820817,0.791637231,1]]
+
+print(summarize_dataset(dataset))

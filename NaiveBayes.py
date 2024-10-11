0d28fe2e828d85bbd249974c9a8c61558d42f057
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-10-11 10:54:42 -0400

    summarize_by_class method
    In this method we separate the datapoints with respect to their classes and then we calculate the mean and stdv for each feature for datapoints for that class

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -37,3 +37,18 @@ def summarize_dataset(dataset):
 dataset = [[3.393533211,2.331273381,0], [3.110073483,1.781539638,0], [1.343808831,3.368360954,0], [3.582294042,4.67917911,0], [2.280362439,2.866990263,0], [7.423436942,4.696522875,1], [5.745051997,3.533989803,1], [9.172168622,2.511101045,1], [7.792783481,3.424088941,1], [7.939820817,0.791637231,1]]
 
 print(summarize_dataset(dataset))
+
+#Summarize data by class
+def summarize_by_class(dataset):
+    separated = sperate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+#Let's test our step 3
+summary = summarize_by_class(dataset)
+for label in summary:
+    print(label)
+    for row in summary[label]:
+        print(row)
